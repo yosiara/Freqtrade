8145f4ab0e4fd4db70cfd6ee5c657d70f6d20148
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2026-04-14 12:51:17 -0400

    fix: path

--- scripts/csv_to_feather.py
+++ scripts/csv_to_feather.py
@@ -1,19 +1,23 @@
-import pandas as pd
 import os
 
+import pandas as pd
+
+
+# ===================== Configuración =========================
+coin_id = 'bitcoin'
+pair = 'BTC_USDT'
+timeframe = '5m'
+from_path = f'user_data/data/coingecko/{coin_id}_ohlcv_5m.csv'
+to_path = f'user_data/data/coinex/{pair}-{timeframe}.feather'
+# =============================================================
+
 # Lee el CSV que generaste
-df = pd.read_csv('bitcoin_ohlcv_5m.csv')
+df = pd.read_csv(from_path)
 df['date'] = pd.to_datetime(df['date'])
 
 # Asegura que tenga el orden de columnas correcto: date, open, high, low, close, volume
 df = df[['date', 'open', 'high', 'low', 'close', 'volume']]
 
-# Define la ruta donde Freqtrade espera los datos
-# Ejemplo: data/coinex/BTC_USDT-1h.feather
-pair = 'BTC_USDT'  # Nombre del par
-timeframe = '5m'
-data_path = f'data/coinex/{pair}-{timeframe}.feather'
-
 # Guarda en formato feather
-df.reset_index(drop=True).to_feather(data_path)
-print(f"Archivo convertido y guardado en: {data_path}")
+df.reset_index(drop=True).to_feather(to_path)
+print(f"Archivo convertido y guardado en: {to_path}")
